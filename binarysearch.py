976b9d4ce3e5a679714c71333e6f847d17052f9b
Nacho-Meter-Stick <138950899+Nacho-Meter-Stick@users.noreply.github.com>
2023-07-07 20:21:24 -0600

    Create binarysearch.py

---
+++ binarysearch.py
@@ -0,0 +1,31 @@
+word = "the"
+file = open('stuff')
+contents = file.read()
+file.close()
+D = contents.split()
+def d_check(wd, d):
+    wd = wd.lower()
+    wd = wd.replace("-", "")
+    A = "!&',./0123456789abcdefghijklmnopqrstuvwxyz"
+    v = [A.find(c) for c in wd]
+    l = 0
+    h = len(d) - 1
+    while l <= h:
+        m = (l + h) // 2
+        w = (d[m]).lower()
+        w = w.replace("-", "")
+        if w == wd:
+            return True
+        if l == h:
+            return False
+        lv = len(v)
+        lw = len(w)
+        mn = min(lv, lw)
+        i = 0
+        while i < (mn) and v[i] == A.find(w[i]):
+            i += 1
+        if i == (lv) or v[i] > A.find(w[i]):
+            l = m + 1
+        else:
+            h = m - 1
+print(d_check(word, D))
