880da5794dceddfd4fea60c7380dfc33e347d1ba
aagowl <abigail.gatsch@gmail.com>
2023-07-28 19:04:30 -0600

    update tree search

--- BK_tree_search.py
+++ BK_tree_search.py
@@ -4,21 +4,22 @@ from tree import dictionary
 
 def searchTree(tree, parent, query, similarWords):
     parentList = tree.get(parent)
-    t = 1
     d = edit_dist(parent, query)
+    tolerence = 2
+
     # if the parent word is in acceptable tolerance t
-    if d <= t:
+    if d <= tolerence:
         similarWords.append(parent)
 
     # Do not have the first index become a negative number
-    if d - t <= 0:
+    if d - tolerence <= 0:
         low = 0
     else:
-        low = d - t
+        low = d - tolerence
 
     # only look at children in acceptable range
     # [edit-dist(parent, query) + t, edit-distance(parent, query) - t]
-    for i in range(low, d + t + 1):
+    for i in range(low, d + tolerence + 1):
         try:
             child = parentList[i + 2][1]
             searchTree(tree, child, query, similarWords)
@@ -28,7 +29,17 @@ def searchTree(tree, parent, query, similarWords):
 
 def returnSimilars(tree, parent, query, similarWords):
   searchTree(tree, parent, query, similarWords)
-  return similarWords
+  finalSuggestions = []
+  finalTol = 1
+
+  # make sure that the final words that are presented are within the
+  # tolerence limit in order to make suggestions more accurate 
+  for i in range(len(similarWords)):
+    if edit_dist(similarWords[i], query) <= finalTol:
+        finalSuggestions.append(similarWords[i])
+
+
+  return finalSuggestions
 
 queryWord = input("What word do you want to check? ").lower().strip()
 print(returnSimilars(dictionary, "stick", queryWord, []))
