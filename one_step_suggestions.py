7615f0735731bf60656bb7b440193bec004f37a2
Nacho-Meter-Stick <138950899+Nacho-Meter-Stick@users.noreply.github.com>
2023-09-01 09:13:00 -0600

    Update one_step_suggestions.py

--- one_step_suggestions.py
+++ one_step_suggestions.py
@@ -1,7 +1,6 @@
 # This code here creates 4 lists, each pertaining to a type of change. ie: insertion, deletion, substitution, and transposition. each list has one or two for-loop(s) behind it, computing all of the permutations and only appending them to the list if d_check(permutation) = True. The final step adds all the lists together into one list, of which equates to [all the existing words one step away].
 # creates (140*len(wd) + 68) words, uses d_check on each one, and appends a word to a list len(suggestions) times, before combining all 4 lists.
 # Theoretically, you could run this program once, and then again for each existing word one step away, for all of the 2-step suggestion words.
-# please do keep in mind, our 'competition' isn't 96% accurate at suggestions or grammar checks, it is (96% or less) accurate at determining whether a word is a word, and marking it correspondingly. Either we are are already at their pace, or we are already at a point where we can discover the hard way, why they are inaccurate.
 from formatted_dictionary import dictionary
 D = dictionary
 
